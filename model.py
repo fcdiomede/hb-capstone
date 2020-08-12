6555d4b4f8f31bf0b7af021999141584f5ff0649
Fernanda Diomede <fcdiomede@gmail.com>
2020-08-12 14:22:12 -0500

    added cookbook class

--- model.py
+++ model.py
@@ -16,10 +16,26 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String, nullable=False)
 
+    cookbook = db.relationship('Cookbook')
+
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
 
+class Cookbook(db.Model):
+    """A cookbook"""
+
+    __tablename__ = 'cookbooks'
+
+    cookbook_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String)
+    cover_img = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return f'<Cookbook cookbook_id={self.cookbook_id} title={self.title}>' 
 
 # Helper functions
 
@@ -41,3 +57,13 @@ if __name__ == "__main__":
     from server import app
     connect_to_db(app)
     db.create_all()
+    
+    #create a test user
+    test_user = User(fname='T', lname='est', email='test@test.test', password='test')
+    db.session.add(test_user)
+    db.session.commit()
+
+    #create a test cookbook
+    test_cookbook = Cookbook(title='Experimental', cover_img='a url', user_id=test_user.user_id)
+    db.session.add(test_cookbook)
+    db.session.commit()
