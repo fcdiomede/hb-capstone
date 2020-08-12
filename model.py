575ecb8d862968d1297f03a6ece225360d0699be
Fernanda Diomede <fcdiomede@gmail.com>
2020-08-12 15:29:51 -0500

    added abstract table between cookbook and recipes

--- model.py
+++ model.py
@@ -28,11 +28,12 @@ class Cookbook(db.Model):
     __tablename__ = 'cookbooks'
 
     cookbook_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
     cover_img = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     user = db.relationship('User')
+    recipes = db.relationship('Recipe', secondary="cookbook_recipes")
 
     def __repr__(self):
         return f'<Cookbook cookbook_id={self.cookbook_id} title={self.title}>'
@@ -43,11 +44,26 @@ class Recipe(db.Model):
     __tablename__ = 'recipes'
 
     recipe_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.Integer)
+    title = db.Column(db.String, nullable=False)
     ingredients = db.Column(db.Text)
+
+    cookbooks = db.relationship('Cookbook', secondary="cookbook_recipes")
 
     def __repr__(self):
         return f'<Recipe recipe_id={self.recipe_id} title={self.title}>'
+
+class CookbookRecipe(db.Model):
+    """Recipes in a specific cookbook."""
+
+    __tablename__ = 'cookbook_recipes'
+
+    cookbook_recipe_id = db.Column(db.Integer, primary_key=True)
+    cookbook_id = db.Column(db.Integer,
+                        db.ForeignKey('cookbooks.cookbook_id'),
+                        nullable=False)
+    recipe_id = db.Column(db.Integer,
+                         db.ForeignKey('recipes.recipe_id'),
+                         nullable=False)
 
 # Helper functions
 
@@ -70,7 +86,7 @@ if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
     
-    #create a test user
+    # #create a test user
     # test_user = User(fname='T', lname='est', email='test@test.test', password='test')
     # db.session.add(test_user)
     # db.session.commit()
@@ -83,4 +99,9 @@ if __name__ == "__main__":
     # #create a test recipe
     # test_recipe= Recipe(title="My first recipe", ingredients="eggs")
     # db.session.add(test_recipe)
+    # db.session.commit()
+
+    # #connect test recipe and test cookbook
+    # cookbook_recipe = CookbookRecipe(cookbook_id= test_cookbook.cookbook_id, recipe_id=test_recipe.recipe_id)
+    # db.session.add(cookbook_recipe)
     # db.session.commit()
